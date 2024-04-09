7b089c0aa7295db4aceb71a822a7319010d49153
qarangym <itsjr@vk.com>
2024-04-10 01:18:42 +0500

    Cryptography and the Solana Network

--- generate-keypair/generate-keypair.ts
+++ generate-keypair/generate-keypair.ts
@@ -1,7 +1,9 @@
-import { Keypair } from "@solana/web3.js";
+import { config } from "dotenv";
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+config();
 
-const keypair = Keypair.generate();
+const keypair = getKeypairFromEnvironment("SECRET_KEY");
 
-console.log(`The public key is: `, keypair.publicKey.toBase58());
-console.log(`The secret key is: `, keypair.secretKey);
-console.log(`✅ Finished!`);
+console.log(
+  `✅ Finished! We've loaded our secret key securely, using an env file!`
+);
